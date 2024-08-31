51298607473bfd4063710fc63bfd68ca53219f86
juliewbech <jube0001@stud.kea.dk>
2024-08-31 15:31:51 +0200

    js new added

---
+++ js/productlist.js
@@ -0,0 +1,24 @@
+fetch("https://kea-alt-del.dk/t7/api/products")
+  .then((res) => res.json())
+  .then(showProducts);
+
+function showProducts(products) {
+  //looper og kalder showProduct
+  products.forEach(showProduct);
+}
+
+function showProduct(product) {
+  console.log(product);
+  //fang template
+  const template = document.querySelector("#smallProductTemplate").content;
+  //lav en kopi
+  const copy = template.cloneNode(true);
+  //ændre indhold
+  copy.querySelector("h3").textContent = product.productdisplayname;
+  if (product.soldout) {
+    //produktet er udsolgt
+    copy.querySelector("article").classList.add("soldOut");
+  }
+  //appende
+  document.querySelector("main").appendChild(copy);
+}
