ed738c9f2a50d2ec09569e3ce831cf9947921e55
juliewbech <jube0001@stud.kea.dk>
2024-09-02 22:17:11 +0200

    js changes

--- js/product.js
+++ js/product.js
@@ -1,12 +1,25 @@
-fetch("https://kea-alt-del.dk/t7/api/products/1525")
-  .then((response) => responds.json())
-  .then((data) => showProduct(data));
+const URLparams = new URLSearchParams(window.location.search);
+const id = URLparams.get("id");
 
-function showProduct(product) {
-  console.log(product);
-  document.querySelector("purchaseBox h3").textContent =
-    product.productdisplayname;
-  document.querySelector("purchaseBox .brand").textContent = product.brandname;
-  document.querySelector("img").src =
-    "https://kea-alt-del.dk/t7/image/webp/640/${product.id}.webp";
+const url = `https://kea-alt-del.dk/t7/api/products/${id}`;
+
+function getProduct() {
+  fetch(url)
+    .then((res) => res.json())
+    .then((product) => {
+      console.log(product.id);
+      document.querySelector(".purchaseBox h3").textContent =
+        product.productdisplayname;
+      document.querySelector(".purchaseBox p").textContent =
+        product.articletype;
+      document.querySelector(".purchaseBox .brand").textContent =
+        product.brandname;
+      document.querySelector(".info").textContent = product.info;
+      document.querySelector(
+        "img"
+      ).src = `https://kea-alt-del.dk/t7/images/webp/640/${id}.webp`;
+      document.querySelector("img").alt =
+        "Sahara Team India Fanwear Round Neck Jersey";
+    });
 }
+getProduct();
