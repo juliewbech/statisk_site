2bd74ae1f8b0c9a8e92004a7ca4b8181913dedfe
juliewbech <jube0001@stud.kea.dk>
2024-09-03 17:59:04 +0200

    classes

--- js/product.js
+++ js/product.js
@@ -10,16 +10,14 @@ function getProduct() {
       console.log(product.id);
       document.querySelector(".purchaseBox h3").textContent =
         product.productdisplayname;
-      document.querySelector(".purchaseBox p").textContent =
-        product.articletype;
-      document.querySelector(".purchaseBox .brand").textContent =
-        product.brandname;
-      document.querySelector(".info").textContent = product.info;
       document.querySelector(
         "img"
       ).src = `https://kea-alt-del.dk/t7/images/webp/640/${id}.webp`;
-      document.querySelector("img").alt =
-        "Sahara Team India Fanwear Round Neck Jersey";
+      document.querySelector("img").alt = product.productdisplayname;
+      document.querySelector(".season").textContent = product.season;
+      document.querySelector(".type").textContent = product.usagetype;
+      document.querySelector(".number").textContent = product.id;
+      document.querySelector(".brand").textContent = product.brandname;
     });
 }
 getProduct();
